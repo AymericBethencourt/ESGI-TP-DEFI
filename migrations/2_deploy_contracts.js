1b102c339929d0814afffa523aa6f5142b79ff5c
AymericBethencourt <aymericb87@gmail.com>
2021-03-19 14:49:05 +0100

    Correction part 1

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,17 @@
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
+const TokenFarm = artifacts.require('TokenFarm')
+
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock DAI Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm)
+  const tokenFarm = await TokenFarm.deployed()
+}
